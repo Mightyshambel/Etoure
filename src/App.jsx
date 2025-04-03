881c22699496f50f99680876d3e319b232753145
TuoNomeUtente <mighty_sham>
2025-04-03 13:16:52 +0200

    tailwind css installation

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Mighty Shambel</h1>
+      <div className="bg-blue-100 min-h-screen p-8">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Tailwind Works! ✅
+      </h1>
+    </div>
     </>
   )
 }
---
+++ src/tailwind.config.js
@@ -0,0 +1,8 @@
+export const content = [
+    "./src/**/*.{js,jsx,ts,tsx}", // Targets all React files
+    "./public/index.html" // Target your HTML file
+];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];
